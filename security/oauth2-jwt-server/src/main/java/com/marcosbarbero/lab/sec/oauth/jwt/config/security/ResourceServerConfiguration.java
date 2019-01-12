9e68b8e213775b8ac74c6e2fc840e52f27704705
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-12 18:43:16 +0100

    Add resource server config

--- security/oauth2-jwt-server/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/config/security/ResourceServerConfiguration.java
+++ security/oauth2-jwt-server/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/config/security/ResourceServerConfiguration.java
@@ -1,6 +1,8 @@
 package com.marcosbarbero.lab.sec.oauth.jwt.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -10,6 +12,8 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+    private static final String ROOT_PATTERN = "/**";
+
     private final TokenStore tokenStore;
 
     public ResourceServerConfiguration(TokenStore tokenStore) {
@@ -17,8 +21,18 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     }
 
     @Override
-    public void configure(final ResourceServerSecurityConfigurer resources) throws Exception {
+    public void configure(final ResourceServerSecurityConfigurer resources) {
         resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, ROOT_PATTERN).access("#oauth2.hasScope('read')")
+                .antMatchers(HttpMethod.POST, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.PATCH, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.PUT, ROOT_PATTERN).access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.DELETE, ROOT_PATTERN).access("#oauth2.hasScope('write')");
     }
 
 }
