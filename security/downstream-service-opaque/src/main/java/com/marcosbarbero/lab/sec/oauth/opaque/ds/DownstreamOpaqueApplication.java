c161bdcd87f8fb23b370cc3afc7aec64debeed87
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-14 13:38:12 +0100

    Add remote token services config

---
+++ security/downstream-service-opaque/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/ds/DownstreamOpaqueApplication.java
@@ -0,0 +1,12 @@
+package com.marcosbarbero.lab.sec.oauth.opaque.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownstreamOpaqueApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(DownstreamOpaqueApplication.class, args);
+    }
+}
---
+++ security/downstream-service-opaque/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/ds/web/DownstreamController.java
@@ -0,0 +1,20 @@
+package com.marcosbarbero.lab.sec.oauth.opaque.ds.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/avengers")
+public class DownstreamController {
+
+    @GetMapping
+    public ResponseEntity<Collection<String>> get() {
+        return ResponseEntity.ok(Arrays.asList("Iron man", "Spider-man", "Thor", "Hulk", "Cap", "Vision"));
+    }
+
+}
