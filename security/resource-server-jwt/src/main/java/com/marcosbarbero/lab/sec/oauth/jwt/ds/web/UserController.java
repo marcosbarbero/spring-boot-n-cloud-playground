41ea33ee998dd1229c020c90c4d6736e54511828
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-23 10:46:17 +0100

    Rename UserController

--- security/resource-server-jwt/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/ds/web/UserController.java
+++ security/resource-server-jwt/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/ds/web/UserController.java
@@ -10,7 +10,7 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/me")
-public class ResourceController {
+public class UserController {
 
     @GetMapping
     @PreAuthorize("hasRole('ROLE_USER')")
