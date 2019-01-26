cc985e9b55b7d0559b8479d4390df4ce21534648
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-26 21:41:48 +0100

    Add OAuth2 access scope

--- security/resource-server-opaque/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/ds/config/ResourceServerConfiguration.java
+++ security/resource-server-opaque/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/ds/config/ResourceServerConfiguration.java
@@ -1,6 +1,8 @@
 package com.marcosbarbero.lab.sec.oauth.opaque.ds.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -8,5 +10,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')")
+                .antMatchers(HttpMethod.POST, "/**").access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.PATCH, "/**").access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.PUT, "/**").access("#oauth2.hasScope('write')")
+                .antMatchers(HttpMethod.DELETE, "/**").access("#oauth2.hasScope('write')");
+    }
 
 }
