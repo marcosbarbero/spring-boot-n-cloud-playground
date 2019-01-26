af90bde802d70c0ef2bfda688b46d02d9747027e
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-26 21:35:34 +0100

    Fix configuration

--- security/oauth2-opaque-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/ResourceServerConfiguration.java
+++ security/oauth2-opaque-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/ResourceServerConfiguration.java
@@ -1,7 +1,6 @@
 package com.marcosbarbero.lab.sec.oauth.opaque.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -20,11 +19,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(final ResourceServerSecurityConfigurer resources) {
         resources.tokenStore(tokenStore);
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers().authenticated();
     }
 
 }
