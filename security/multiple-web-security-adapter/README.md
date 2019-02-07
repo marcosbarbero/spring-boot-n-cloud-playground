173cc58d578007071da3acefddecee17343a10ec
Marcos Barbero <marcos.hgb@gmail.com>
2019-02-07 19:54:03 +0100

    Add multiple security adapter project

---
+++ security/multiple-web-security-adapter/README.md
@@ -0,0 +1,2 @@
+Multiple Web Security Adapter
+---
---
+++ security/multiple-web-security-adapter/src/main/java/com/marcosbarbero/lab/sec/multiple/adapters/MultipleWebSecurityAdapterApplication.java
@@ -0,0 +1,12 @@
+package com.marcosbarbero.lab.sec.multiple.adapters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleWebSecurityAdapterApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(MultipleWebSecurityAdapterApplication.class, args);
+    }
+}
