312e5354f67b7cd498b22ce7ced26bfa87725ba9
Marcos Barbero <marcos.hgb@gmail.com>
2019-02-08 20:25:11 +0100

    Override `userDetailsService` method

--- security/oauth2-opaque-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/WebSecurityConfiguration.java
+++ security/oauth2-opaque-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/WebSecurityConfiguration.java
@@ -45,6 +45,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Override
     public UserDetailsService userDetailsService() {
         if (userDetailsService == null) {
             userDetailsService = new JdbcDaoImpl();
